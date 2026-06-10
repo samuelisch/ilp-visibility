e45bf3f5e88ec35435ec524d59f9dd292c9c2691
samuelisch <samuelchanwk@hotmail.com>
2026-06-10 20:21:54 +0800

    cleanup jest stuff

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -15,9 +15,7 @@ export default tseslint.config(
     languageOptions: {
       globals: {
         ...globals.node,
-        ...globals.jest,
       },
-      sourceType: 'commonjs',
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
