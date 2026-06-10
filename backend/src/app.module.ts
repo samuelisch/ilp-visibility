cf3cbfd1637f5e581796cbc81f7df53558c015c8
samuelisch <samuelchanwk@hotmail.com>
2026-06-11 00:17:19 +0800

    remove boilerplate

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma.module.js';
 import { PolicyModule } from './policy/policy.module.js';
 
 @Module({
   imports: [PrismaModule, PolicyModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
