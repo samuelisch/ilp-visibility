6f5fd187ae4fefcd542a446ab32e88abeca8f398
samuelisch <samuelchanwk@hotmail.com>
2026-06-17 15:48:39 +0800

    feat(ui): load fonts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
+import '@fontsource-variable/fraunces';
+import '@fontsource-variable/hanken-grotesk';
 import './index.css';
 import App from './App.tsx';
 
